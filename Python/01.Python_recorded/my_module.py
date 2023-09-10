dedf1875f2a60bd30ba95c5a25bb1036e70c02ac
Sandeep Bandi <38294668+sandeepbandi924@users.noreply.github.com>
2023-09-10 14:01:17 +0530

    Add files via upload

---
+++ Python/01.Python_recorded/my_module.py
@@ -0,0 +1,12 @@
+data = { 'name':'ineuron',
+        'course': ['FSDS','Blockchain','Data Science', 'ML','Cloud'],
+        'greetings': 'Greetings from Ineuron'
+        
+}
+
+def get_course():
+    'Coureses from Ineuron'
+    return data['course']
+
+def grettings():
+    return data['greetings']
---
+++ Python/01.Python_recorded/test.py
@@ -0,0 +1,14 @@
+def test(a,b):
+    """ This is a function will return Adddition of two numbers"""
+	return a+b
+
+def test1(b,c):
+	""" This is a function will return multiplcation of two numbers"""
+	return b * c
+	
+def test2(d,e) :
+    """ This is a function will return division of two numbers"""
+	return d / e
+
+def test4():
+	print('this is my code written in module test')
